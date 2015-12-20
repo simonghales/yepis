b31f9261c1c6002286055d47bb6760f51ec8eaf9
simonghales <simonghales@gmail.com>
2015-12-20 23:55:32 +1000

    started work on js to resize images and centre

--- src/app/story/page/story.page.directive.js
+++ src/app/story/page/story.page.directive.js
@@ -5,16 +5,37 @@
     .module('yepis.story.directives')
     .directive('storyPage', storyPage);
 
-  function storyPage() {
+  function storyPage($log, $window) {
 
     var directive = {
       restrict: 'E',
       templateUrl: 'app/story/page/_story.page.html',
       replace: true,
-      scope: false
+      scope: false,
+      link: link
     }
 
     return directive;
+
+    function link(scope, element, attributes) {
+
+      var img = element[0].querySelector('.story-page__bg-image');
+
+      resizePage();
+
+      scope.$on('resize-pages', function() {
+        resizePage();
+      });
+
+      function resizePage() {
+
+        element.css({
+          'height' : $window.innerHeight + 'px'
+        });
+
+      }
+
+    }
 
   }
 
